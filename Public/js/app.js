6efeff70b4381d3a2031241f2597cee1721ae8fc
JT Parker <justin.parker@utahradiology.com>
2016-06-19 22:35:44 -0600

    app functioning

--- Public/js/app.js
+++ Public/js/app.js
@@ -2,27 +2,29 @@ var app = angular.module('beerFinder', ['ui.router']);
 
 app.config([
   "$stateProvider",
-  "$urlRouteProvider",
+  "$urlRouterProvider",
 
-  function($stateProvider, $urlRouteProvider) {
+  function($stateProvider, $urlRouterProvider) {
 
-    $urlRouteProvider.otherwise('./main');
+    $urlRouterProvider.otherwise('/');
 
-    $stateProvider.state('main', {
-      url:'/main',
-      templateUrl: './views/main.html'
-    })
-    .state('findBeer', {
-      url:'/findBeer',
-      templateUrl: './views/findBeer.html'
-    })
-    .state('findStores', {
-      url: './findStores',
-      templateUrl: './views/findStores.html'
-    })
-    .state('reportFinding', {
-      url: './reportFinding',
-      templateUrl: './views/reportFinding.html'
-    })
-  }
+    $stateProvider
+      .state('main', {
+        url:'/',
+        templateUrl: './views/main.html',
+        controller: 'mainCtrl'
+      })
+      .state('findBeer', {
+        url:'/findBeer',
+        templateUrl: './views/findBeer.html'
+      })
+      .state('findStores', {
+        url: '/findStores',
+        templateUrl: './views/findStores.html'
+      })
+      .state('reportFinding', {
+        url: '/reportFinding',
+        templateUrl: './views/reportFinding.html'
+      })
+    }
 ]);
---
+++ Public/js/controllers/mainCtrl.js
@@ -0,0 +1,13 @@
+app.controller('mainCtrl', function($scope) {
+
+
+$scope.test = "It works!";
+
+
+
+
+
+
+
+
+});
